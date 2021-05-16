f4bd130fe969ee2b809e3ce5cafb6cbf4af6919e
Vignesh Suresh Kumar <vigneshsureshkumar17@gmail.com>
2021-05-16 17:25:05 -0400

    Of course I forgot something 🤦‍

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -21,7 +21,7 @@ class App : Application() {
     val endRect = Rectangle(100.0, 100.0, 10.0, 10.0)
 
     var startTime = Double.NaN
-    val trajectories = TrajectoryGenSquare.createTrajectory()
+    val trajectories = TrajectoryGen.createTrajectory()
 
     lateinit var fieldImage: Image
     lateinit var stage: Stage
